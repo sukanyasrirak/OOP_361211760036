4d8b8cdd77ff1da04900cb6185d8b133b3b3be25
sukanyasrirak <40585939+sukanyasrirak@users.noreply.github.com>
2019-12-08 11:22:12 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
         System.out.println("Hello World,I am PONLY");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
+         System.out.println("Saiyai");
 
 
 
