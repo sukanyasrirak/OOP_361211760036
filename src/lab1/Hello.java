a95f32ac9d5b612503c9e92c0d8b2442c94841e3
Sukanya <sukanyasrirak220740@gmail.com>
2019-12-08 11:30:13 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
         System.out.println("Faculty of Management Technology");
         System.out.println("Saiyai");
         System.out.println("Krabi");
+        System.out.println("ponly");
 
 
 
