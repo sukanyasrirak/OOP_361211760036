6ef285c00142e4cbb5122d9ce6275fb303bcc7b8
Sukanya <sukanyasrirak220740@gmail.com>
2019-12-08 11:11:31 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World,I am PONLY");
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
 
 
 
