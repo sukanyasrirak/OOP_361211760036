f48018506e604319bf10b544751d255640efd7d8
Sukanya <sukanyasrirak220740@gmail.com>
2019-12-11 10:49:30 +0700

    Add basicJava2

---
+++ src/lab2/basicJava2.java
@@ -0,0 +1,34 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class basicJava2 {
+
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        String name = sc.nextLine();
+//        System.out.println("Your name is " +name);
+//
+//        System.out.print("Enter your email: ");
+//        String email = sc.nextLine();
+//        System.out.println("Your email is " +email);
+//
+//        System.out.print("Enter your age: ");
+//        String age = sc.nextLine();
+//        System.out.println("Your age is " +age);
+
+        //input integer and floating
+        System.out.print("Enter an number: ");
+        int num = sc.nextInt();
+        System.out.println("Your entered number: "+num);
+
+        System.out.print("Enter a real number: ");
+        double d = sc.nextDouble();
+        System.out.println(d);
+
+    }
+
+}//class
